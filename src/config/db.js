88a96de759454dcb8f0fcf81431fc82f5bdf0085
Jhn8411 <joao.caetano2784@gmail.com>
2026-05-15 17:03:04 -0300

    atualização db (15/05)

--- src/config/db.js
+++ src/config/db.js
@@ -3,18 +3,16 @@ require('dotenv').config();
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   database: process.env.DB_NAME,
-});
-
-// Testando a conexão inicial
-pool.on('connect', () => {
-  console.log('📦 Conexão com o banco de dados PostgreSQL estabelecida com sucesso!');
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  // ADICIONE EXATAMENTE ESTAS 3 LINHAS ABAIXO 👇
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = {
-  // Exportamos o método query para usarmos nos nossos Repositories/Models
   query: (text, params) => pool.query(text, params),
 };
